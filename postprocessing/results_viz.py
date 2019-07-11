0d8ebbb24179a50a2c7286cbed04c279e455f152
Hoang Nghia Tuyen <hnt4499@gmail.com>
2019-07-11 18:32:40 +0700

    Add basic backbone (havent tested carefully)

--- postprocessing/results_viz.py
+++ postprocessing/results_viz.py
@@ -5,6 +5,22 @@ the results.
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+from .utils.vis_util import filter_outliers
+
+
+def get_x(num_steps, step):
+    return np.arange(start=step, stop=step * num_steps + 1, step=step)
+
+
+def get_minmax(x1, x2, mode):
+    # If both are not None
+    if x1 and x2:
+        return mode(x1, x2)
+    # If both is None
+    elif (not x1) and (not x2):
+        return None
+    else:
+        return x1 if x1 else x2
 
 
 def main(args):
@@ -12,8 +28,47 @@ def main(args):
     fig_size = (args.fig_width, args.fig_height)
     train_xmin = args.train_xmin
     train_xmax = args.train_xmax
+    train_step = args.train_step
     val_xmin = args.val_xmin
     val_xmax = args.val_xmax
+    val_step = args.val_step
+    # Compare type
+    assert isinstance(train_xmin, type(train_xmax))
+    assert isinstance(val_xmin, type(val_xmax))
+    # Load json
+    with open(args.input_json, "r") as j:
+        data = json.load(j)
+
+    train_y = np.asarray(data["train_loss_history"], dtype=np.float32)
+    val_y = np.asarray(data["val_loss_history"], dtype=np.float32)
+    if args.ignore_outliers:
+        train_y = filter_outliers(train_y, thresh=args.threshold)
+        val_y = filter_outliers(val_y, thresh=args.threshold)
+
+    train_x = get_x(num_steps=train_y.shape[0], step=1)
+    val_x = get_x(num_steps=val_y.shape[0], step=data["checkpoint_every"])
+    # Plot figure(s)
+    fig = plt.figure(figsize=fig_size)
+    # If single mode is activated
+    if args.single:
+        xmin = get_minmax(train_xmin, val_xmin, min)
+        xmax = get_minmax(train_xmax, val_xmax, max)
+        # Set min and max value of axes if both are specified
+        plt.xlim(left=xmin, right=xmax)
+        plt.plot(train_x[::train_step], train_y[::train_step])
+        plt.plot(val_x[::val_step], val_y[::val_step])
+    # Else, plot two subplots
+    else:
+        # Plot train data
+        ax1 = plt.subplot(121)
+        ax1.set_xlim(left=train_xmin, right=train_xmax)
+        ax1.plot(train_x[::train_step], train_y[::train_step])
+        # Plot val data
+        ax2 = plt.subplot(122)
+        ax2.set_xlim(left=val_xmin, right=val_xmax)
+        ax2.plot(val_x[::val_step], val_y[::val_step])
+    # Save figure
+    plt.savefig(fname=args.output_file, dpi=args.dpi)
 
 
 def parse_arguments(argv):
