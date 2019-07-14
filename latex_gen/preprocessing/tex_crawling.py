3a05c266a476029e360203563e09e56e8de8a9ce
Hoang Nghia Tuyen <hnt4499@gmail.com>
2019-07-14 16:49:04 +0800

    Make it able to work with directory

--- latex_gen/preprocessing/tex_crawling.py
+++ latex_gen/preprocessing/tex_crawling.py
@@ -1,5 +1,5 @@
-DESCRIPTION = """This script extracts LaTeX/TeX formulas from a regular
-text file.
+DESCRIPTION = """This script extracts LaTeX/TeX formulas from all regular
+text files contained within the same folder.
 """
 
 import os
@@ -7,6 +7,7 @@ import sys
 import argparse
 import re
 
+from tqdm import tqdm
 
 REGEX_PATTERN = r"\\(?:begin\{equation\*?\})\s?(?:\\(?:begin|label)\{(?:.*?)\}\s?)*\s*(.*?)\s*\\end\{(?:equation\*?|split)\}"
 # The below regex pattern is used to find small, in-line formulas
@@ -20,19 +21,24 @@ def main(args):
     min_len = args.min_len
     max_len = args.max_len
     # Read input data
-    with open(text_input, "r", encoding=args.encoding) as file:
-        data = file.read().replace("\n", " ") # replace newline char with space.
-    # Use regular expression to extract data
-    formulas = re.findall(REGEX_PATTERN, data)
-    small_formulas = re.findall(REGEX_PATTERN_2, data)
-    # Write output data
     with open(text_output, "w") as out:
-        for formula in formulas:
-            if min_len <= len(formula) <= max_len:
-                out.write(formula + "\n")
-        for formula in small_formulas:
-            if min_len <= len(formula) <= max_len:
-                out.write(formula + "\n")
+        for file_name in tqdm(os.listdir(text_input)):
+            file_path = os.path.join(text_input, file_name)
+            # Skip directories
+            if os.path.isdir(file_path):
+                continue
+            with open(file_path, "r", encoding=args.encoding) as file:
+                data = file.read().replace("\n", " ") # replace newline char with space.
+            # Use regular expression to extract data
+            formulas = re.findall(REGEX_PATTERN, data)
+            small_formulas = re.findall(REGEX_PATTERN_2, data)
+            # Write output data
+            for formula in formulas:
+                if min_len <= len(formula) <= max_len:
+                    out.write(formula + "\n")
+            for formula in small_formulas:
+                if min_len <= len(formula) <= max_len:
+                    out.write(formula + "\n")
 
 
 def parse_arguments(argv):
@@ -41,7 +47,7 @@ def parse_arguments(argv):
         description=DESCRIPTION)
 
     parser.add_argument('text_input', type=str,
-        help='Path to the input text file.')
+        help='Directory to the input text files.')
     parser.add_argument('text_output', type=str,
         help='Path to the output text file.')
 
